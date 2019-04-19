e6da184418f832afaa645ce19557f85bd5d68d80
Colin Walters <walters@verbum.org>
2019-04-19 20:50:34 +0000

    wip

--- src/coretoolbox.rs
+++ src/coretoolbox.rs
@@ -1,10 +1,13 @@
 use structopt::StructOpt;
 use std::path::Path;
-use std::ffi::{CString, OsString};
 use std::process::{Command, Stdio};
+use std::os::unix::process::CommandExt;
+use std::io::prelude::*;
 use directories;
 use failure::{Fallible, bail};
 use lazy_static::lazy_static;
+use serde_json;
+use serde::{Serialize, Deserialize};
 
 lazy_static! {
     static ref APPDIRS : directories::ProjectDirs = directories::ProjectDirs::from("com", "coreos", "toolbox").expect("creating appdirs");
@@ -64,12 +67,25 @@ fn is_ostree_based_host() -> bool {
     std::path::Path::new("/run/ostree-booted").exists()
 }
 
-/// Pull a container image if not present
-fn ensure_image(name: &str) -> Fallible<()> {
-    if !cmd_podman().args(&["inspect", name])
+enum InspectType {
+    Container,
+    Image,
+}
+
+fn podman_has(t: InspectType, name: &str) -> Fallible<bool> {
+    let typearg = match t {
+        InspectType::Container => "container",
+        InspectType::Image => "image",
+    };
+    Ok(cmd_podman().args(&["inspect", "--type", typearg, name])
         .stdout(Stdio::null())
         .stderr(Stdio::null())
-        .status()?.success() {
+        .status()?.success())
+}
+
+/// Pull a container image if not present
+fn ensure_image(name: &str) -> Fallible<()> {
+    if !podman_has(InspectType::Image, name)? {
         if !cmd_podman().args(&["pull", name]).status()?.success() {
             bail!("Failed to pull image");
         }
@@ -85,6 +101,13 @@ fn getenv_required_utf8(n: &str) -> Fallible<String> {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct EntrypointState {
+    username: String,
+    uid: u32,
+    ostree_based_host: bool,
+}
+
 fn run(opts: Opt) -> Fallible<()> {
     ensure_image(&opts.image)?;
 
@@ -92,6 +115,12 @@ fn run(opts: Opt) -> Fallible<()> {
     // would be better with podman fd passing.
     let self_bin = std::fs::read_link("/proc/self/exe")?;
     let self_bin = self_bin.as_path().to_str().ok_or_else(|| failure::err_msg("non-UTF8 self"))?;
+
+    // Serialize our
+    let pid : i32 = nix::unistd::getpid().as_raw().into();
+    let runtime_dir = getenv_required_utf8("XDG_RUNTIME_DIR")?;
+    let r : u32 = rand::random();
+    let statefile = format!("toolbox-data-{}-{:x}", pid, r);
 
     let mut podman = cmd_podman();
     podman.args(&["run", "--rm", "-ti", "--hostname", "toolbox",
@@ -128,36 +157,70 @@ fn run(opts: Opt) -> Fallible<()> {
         let v = v.to_str().ok_or_else(|| failure::format_err!("{} contains invalid UTF-8", n))?;
         podman.arg(format!("--env={}={}", n, v));
     }
+    podman.arg(format!("--env=TOOLBOX_STATEFILE={}", statefile));
+
+    {
+        let state = EntrypointState {
+            username: getenv_required_utf8("USER")?,
+            uid: real_uid,
+            ostree_based_host: is_ostree_based_host(),
+        };
+        let w = std::fs::File::create(format!("{}/{}", runtime_dir, statefile))?;
+        let mut w = std::io::BufWriter::new(w);
+        serde_json::to_writer(&mut w, &state)?;
+        w.flush()?;
+    }
+
     podman.arg("--entrypoint=/toolbox.entrypoint");
     podman.arg(opts.image);
     eprintln!("running {:?}", podman);
-    if !podman.status()?.success() {
-        bail!("podman failed");
-    }
-    Ok(())
+    return Err(podman.exec().into())
 }
 
-fn entrypoint() -> Fallible<()> {
-    let shell = std::env::var_os("SHELL").unwrap_or("sh".into())
-        .into_string().map_err(|e| failure::err_msg("Invalid SHELL"))?;
-    let shell = CString::new(shell)?;
-    nix::unistd::execvp(&shell, &[]).ok();
-    let sh = CString::new("sh")?;
-    nix::unistd::execvp(&sh, &[])?;
-    Ok(())
+mod entrypoint {
+    use failure::{Fallible, bail};
+    use std::process::Command;
+    use std::os::unix::process::CommandExt;
+
+    fn adduser(name: &str, uid: u32) -> Fallible<()> {
+        let uidstr = format!("{}", uid);
+        if !Command::new("useradd")
+            .args(&["--no-create-home", "--uid", &uidstr,
+                    "--groups", "wheel", name])
+            .status()?.success() {
+                bail!("Failed to useradd");
+        }
+        Ok(())
+    }
+
+    pub(crate) fn entrypoint() -> Fallible<()> {
+        let statefile = super::getenv_required_utf8("TOOLBOX_STATEFILE")?;
+        let runtime_dir = super::getenv_required_utf8("XDG_RUNTIME_DIR")?;
+        let state : super::EntrypointState = {
+            let f = std::fs::File::open(format!("/host/{}/{}", runtime_dir, statefile))?;
+            serde_json::from_reader(std::io::BufReader::new(f))?
+        };
+
+        adduser(&state.username, state.uid)?;
+
+        let shell = std::env::var_os("SHELL").unwrap_or("sh".into())
+            .into_string().map_err(|_| failure::err_msg("Invalid SHELL"))?;
+        Command::new(shell).exec();
+        Err(Command::new("sh").exec().into())
+    }
 }
 
 /// Primary entrypoint
 fn main() -> Fallible<()> {
     let argv0 = std::env::args().next().expect("argv0");
     if argv0.ends_with(".entrypoint") {
-        return entrypoint();
+        return entrypoint::entrypoint();
     }
     let opts = Opt::from_args();
     if let Some(cmd) = opts.cmd.as_ref() {
         match cmd {
             Cmd::Entrypoint => {
-                return entrypoint();
+                return entrypoint::entrypoint();
             }
         }
     } else {
